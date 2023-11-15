f85198f83866ffc17ec629a409d070e266cc321e
abhi-sj <abhisree76@gmail.com>
2023-11-15 05:53:43 +0530

    test1

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,9 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import "./footer.css";
-import { BsFacebook } from "react-icons/bs";
-import { BsInstagram } from "react-icons/bs";
-import { BsTwitter } from "react-icons/bs";
 
 const Footer = () => {
   return (
